3520d953caf0cff13ee26791f0a97af49cd4f5c8
YeonJin Shin <83462564+Yeonjin-Shin@users.noreply.github.com>
2022-05-26 10:52:08 +0800

    Add files via upload

--- board/views.py
+++ board/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def board(request):
+    if request.method == "GET":
+        context = {
+            "title":"안녕 누구?"
+        }
+        return render(request, 'page/index.html', context=context)
